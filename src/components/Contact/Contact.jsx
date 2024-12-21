1bcb91daab94b1d837862ef264827164575e9ed1
breachblockco <breachblockco@gmail.com>
2024-12-21 18:18:17 +0530

    footer completed

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,35 +3,59 @@ const Contact = () => {
    
  
   return (
-   
-    <div  className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden   bg-[#D4D4D4] text-black' id="Contact Us">
-    <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center '>Contact <span className='underline underline-offset-4 decoration-1 under font-light  text-[#4361EE]'>With Us</span></h1>
-    <p className='text-center text-black mb-12 max-w-80 mx-auto '> Need Reliable Power Solutions? Reach Out to Us Today and Keep Your Operations Running Smoothly with Our Expert Generator Services.</p>
- <form  className='max-w-2xl mx-auto text-gray-600 pt-8  bg-white shadow-lg rounded-lg p-8'>
-  <div className='flex flex-wrap'>
-    <div className='w-full md:w-1/2 text-left text-[#000000]'>
-     Your Name
-     <input className='w-full border border-gray-300 rounded py-3 px-4 mt-2' type="text" name='Name' placeholder='Your Name' required/>
-
+    <div
+      className="text-center p-6 py-20 lg:px-32 w-full overflow-hidden   bg-[#F7F7F7] text-black"
+      id="Contact Us"
+    >
+      <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center ">
+        Contact{" "}
+        <span className="underline underline-offset-4 decoration-1 under font-light  text-[#4361EE]">
+          With Us
+        </span>
+      </h1>
+      <p className="text-center text-black mb-12 max-w-80 mx-auto ">
+        {" "}
+        Need Reliable Power Solutions? Reach Out to Us Today and Keep Your
+        Operations Running Smoothly with Our Expert Generator Services.
+      </p>
+      <form className="max-w-2xl mx-auto text-gray-600 pt-8  bg-white shadow-lg rounded-lg p-8">
+        <div className="flex flex-wrap">
+          <div className="w-full md:w-1/2 text-left text-[#000000]">
+            Your Name
+            <input
+              className="w-full border border-gray-300 rounded py-3 px-4 mt-2"
+              type="text"
+              name="Name"
+              placeholder="Your Name"
+              required
+            />
+          </div>
+          <div className="w-full md:w-1/2 text-left md:pl-4 text-[#000000]">
+            Your Email
+            <input
+              className="w-full border border-gray-300 rounded py-3 px-4 mt-2"
+              type="email"
+              name="Email"
+              placeholder="Your Email"
+              required
+            />
+          </div>
+        </div>
+        <div className="my-6 text-left text-[#000000]">
+          Message
+          <textarea
+            className="w-full border border-gray-300 text-black rounded py-3 px-4 mt-2 h-48 resize-none"
+            name="Message"
+            placeholder="Message"
+            required
+          ></textarea>
+        </div>
+        <button className="bg-blue-600 text-white px-8 py-2 rounded transition-colors duration-200 hover:bg-black">
+          Submit
+        </button>
+      </form>
     </div>
-    <div className='w-full md:w-1/2 text-left md:pl-4 text-[#000000]'>
-    Your Email
-    <input className='w-full border border-gray-300 rounded py-3 px-4 mt-2' type="email" name='Email' placeholder='Your Email' required/>
-    </div>
- </div>
- <div className='my-6 text-left text-[#000000]'>
-    Message
-    <textarea className='w-full border border-gray-300 text-black rounded py-3 px-4 mt-2 h-48 resize-none'
-    name="Message" placeholder='Message' required></textarea>
- </div>
- <button
-        className="bg-blue-600 text-white px-8 py-2 rounded transition-colors duration-200 hover:bg-black"
-      >
-       Submit
-      </button>
-</form> 
-</div>
-)
+  );
 }
 
 export default Contact
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,22 +1,70 @@
 import React from 'react'
+import { IoLocationSharp } from "react-icons/io5";
+import { IoMail } from "react-icons/io5";
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className=" overflow-hidden w-full bg-white py-16" id="Footer">
-      <div className="w-[30%] h-[500px] bg-red-300">
-        <div className="w-[10vw] h-[20vh] ">
+    <div
+      className=" overflow-hidden w-full bg-[#F7F7F7] mt-16 px-14 flex h-[350px] gap-20"
+      id="Footer"
+    >
+      <div className="w-[30%] h-full">
+        <div className="w-[10vw] h-[20vh] -ml-5">
           <img
             src="/images/navbar/logo.svg"
             className="w-full h-full "
             alt=""
           />
         </div>
-        <h5 className=''>
+        <h5>
           At Nexxus Power Systems, we specialize in delivering reliable
           generator rental solutions for industrial and domestic needs. Backed
           by world-class brands, we ensure uninterrupted power and exceptional
           service. Your power, our priority
         </h5>
+      </div>
+      <div className="w-[15%] h-full flex flex-col mt-12 gap-5">
+        <h5 className="text-2xl font-bold capitalize text-blue-700 nunito-family">
+          Quick Links
+        </h5>
+        <div className="text-lg nunito-family font-semibold flex flex-col gap-1">
+          <h6>Home</h6>
+          <h6>Services</h6>
+          <h6>Products</h6>
+          <h6>About Us</h6>
+        </div>
+      </div>
+      <div className="w-[20%] h-full flex flex-col mt-12 gap-5">
+        <h5 className="text-2xl font-bold capitalize text-blue-700 nunito-family">
+          Contact Info
+        </h5>
+        <div className="flex gap-1">
+          <IoLocationSharp size={20} className="mt-1" />
+          <p className="leading-[22px]">
+            At. Rajwada, Ward No. 03,
+            <br />
+            Nr. Shiv Mandir, Sahebganj,
+            <br />
+            Dist. Muzaffarpur, Bihar-843125
+          </p>
+        </div>
+        <div className="flex gap-1.5 mt-2">
+          <IoMail size={20} />
+          <p className="leading-none">Info@nexxus.org.in</p>
+        </div>
+      </div>
+      <div className="w-[20%] h-full flex flex-col mt-12 gap-5">
+        <h5 className="text-2xl font-bold capitalize text-blue-700 nunito-family">
+          Follow Us
+        </h5>
+        <div className="flex w-full justify-start gap-8">
+          <FaInstagram size={34} />
+          <FaFacebookSquare size={34} />
+          <FaTwitter size={34} />
+        </div>
       </div>
     </div>
   );
