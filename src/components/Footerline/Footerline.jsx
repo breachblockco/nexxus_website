c20b16c45c30b555cb779e48d34e254c264f1783
Yogita <yogita.verma@breachblockco>
2025-01-08 11:23:03 +0530

    add footerline

---
+++ src/components/Footerline/Footerline.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footerline = () => {
+  return (
+    <div className="border-t border-gray-700 mt-2 pt-2 text-center text-sm">
+     @Design and Developed by Breachblock Co.<br />
+    
+  </div>
+  )
+}
+
+export default Footerline
